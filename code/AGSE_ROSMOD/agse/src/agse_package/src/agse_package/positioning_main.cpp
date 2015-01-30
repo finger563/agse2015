b88acec525e1b5ff69f31bb10897c61cf02b5193
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-01-30 23:18:33 +0000

    Updated model for serial thread concerns

--- code/AGSE_ROSMOD/agse/src/agse_package/src/agse_package/positioning_main.cpp
+++ code/AGSE_ROSMOD/agse/src/agse_package/src/agse_package/positioning_main.cpp
@@ -8,6 +8,7 @@
 #include "agse_package/radial_actuator_controller.hpp" 
 #include "agse_package/vertical_actuator_controller.hpp" 
 #include "agse_package/arm_rotation_controller.hpp" 
+#include "agse_package/gripper_controller.hpp" 
 
 
 void componentThread(Component* compPtr)
@@ -28,6 +29,7 @@ int main(int argc, char **argv)
     radial_actuator_controller radial_controller_i; 
     vertical_actuator_controller vertical_controller_i; 
     arm_rotation_controller rotational_controller_i; 
+    gripper_controller gripper_controller_i; 
 
     // Create Component Threads
     boost::thread radial_controller_i_thread(componentThread, &radial_controller_i);
@@ -36,6 +38,8 @@ int main(int argc, char **argv)
     ROS_INFO("Node positioning has started vertical_controller_i");
     boost::thread rotational_controller_i_thread(componentThread, &rotational_controller_i);
     ROS_INFO("Node positioning has started rotational_controller_i");
+    boost::thread gripper_controller_i_thread(componentThread, &gripper_controller_i);
+    ROS_INFO("Node positioning has started gripper_controller_i");
 
 
     ROS_INFO_STREAM("NodeMain thread id = " << boost::this_thread::get_id());
@@ -44,6 +48,7 @@ int main(int argc, char **argv)
     radial_controller_i_thread.join();
     vertical_controller_i_thread.join();
     rotational_controller_i_thread.join();
+    gripper_controller_i_thread.join();
 
 
     return 0; 
