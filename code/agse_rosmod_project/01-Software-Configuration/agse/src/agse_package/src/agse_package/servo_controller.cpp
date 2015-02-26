47569a25827aa38ac7330ecb00723bd58d8fb880
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2015-02-26 00:54:18 +0000

    Updated CMakeLists - working opencv

--- code/agse_rosmod_project/01-Software-Configuration/agse/src/agse_package/src/agse_package/servo_controller.cpp
+++ code/agse_rosmod_project/01-Software-Configuration/agse/src/agse_package/src/agse_package/servo_controller.cpp
@@ -14,7 +14,7 @@ void servo_controller::Init(const ros::TimerEvent& event)
 {
     // Initialize Component
   paused = true;
-  sprintf(portName,"//dev//ttyTHS0");
+  sprintf(portName,"//dev//ttyO5");
   armServoID = 1;
   gripperRotationID = 10;
   gripperPositionID = 11;
@@ -91,7 +91,11 @@ bool servo_controller::gripperRotationCallback(agse_package::gripperRotation::Re
 void servo_controller::servoTimerCallback(const ros::TimerEvent& event)
 {
     // Business Logic for servoTimer 
-  if (!paused) {
+  if (paused) {
+
+    armRotationGoal = Dynamixel::posToAngle( rand() % 1023 );
+    gripperRotationGoal = Dynamixel::posToAngle( rand() % 1023 );
+    gripperPosGoal = Dynamixel::posToAngle( rand() % 1023 );
 
     int pos; // temp value to store position from servo
     
