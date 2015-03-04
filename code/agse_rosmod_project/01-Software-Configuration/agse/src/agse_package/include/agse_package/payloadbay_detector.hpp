a08022592cec9c85613ba0841a12067896addb9b
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2015-03-04 13:55:22 +0000

    Updated detector and image processing code.  Now have two objects in image_processor component: sampleDetector and payloadBayDetector

---
+++ code/agse_rosmod_project/01-Software-Configuration/agse/src/agse_package/include/agse_package/payloadbay_detector.hpp
@@ -0,0 +1,24 @@
+#ifndef PAYLOADBAY_DETECTOR_HPP
+#define PAYLOADBAY_DETECTOR_HPP
+
+#include <iostream>
+#include "aruco/aruco.h"
+#include "aruco/cvdrawingutils.h"
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace cv;
+using namespace aruco;
+
+class PayloadBay_Detector {
+public:
+  void init(float msize, const char* camParamFileName);
+  std::vector<Marker> run(std::vector<unsigned char> & raw_image_data, int width, int height, const char* fname = "");
+private:
+  MarkerDetector MDetector;
+  // real-world size of the markers
+  float MarkerSize;
+  // the read-in yml file which describes camera intrinsics
+  aruco::CameraParameters CamParam;
+};
+
+#endif
